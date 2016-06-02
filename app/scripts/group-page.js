f34a67fdd63c3838b3813282457650ec3c0338c2
Bernie <b4456609@gmail.com>
2016-06-02 21:59:00 +0800

    change group organize

---
+++ app/scripts/group-page.js
@@ -0,0 +1,26 @@
+
+
+myApp.onPageInit('group', function (page) {
+  // Fruits data demo array
+  var fruits = ('Apple Apricot Avocado Banana Melon Orange Peach Pear Pineapple').split(' ');
+
+  var autocompleteDropdownExpand = myApp.autocomplete({
+    input: '#autocomplete-dropdown-expand',
+    openIn: 'dropdown',
+    expandInput: true, // expand input
+    source: function (autocomplete, query, render) {
+      console.log('t');
+      var results = [];
+      if (query.length === 0) {
+        render(results);
+        return;
+      }
+      // Find matched items
+      for (var i = 0; i < fruits.length; i++) {
+        if (fruits[i].toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(fruits[i]);
+      }
+      // Render items by passing array with result items
+      render(results);
+    }
+  });
+});
