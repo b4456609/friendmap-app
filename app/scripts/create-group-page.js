b749f7cdd1e0d812e130b698c0094817d16b2b67
Bernie <b4456609@gmail.com>
2016-06-03 11:40:12 +0800

    add notification after create group

--- app/scripts/create-group-page.js
+++ app/scripts/create-group-page.js
@@ -1,3 +1,22 @@
 myApp.onPageInit('create-group', function (page) {
-  // Fruits data demo array
+
 });
+
+function onGroupSuccess() {
+  myApp.addNotification({
+    hold: 2000,
+    title: '建立成功',
+    message: '現在可以加入新的成員了',
+    onClose: function () {
+      mainView.router.loadPage('group.html');
+    }
+  });
+}
+
+function onGroupfail() {
+  myApp.addNotification({
+    hold: 2000,
+    title: '建立失敗',
+    message: '請檢查連線狀態，稍候在重試'
+  });
+}
