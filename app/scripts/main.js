b03e3e565b8ae0e16363f24548e482b25f3a3aa5
Bernie <b4456609@gmail.com>
2016-06-03 11:22:26 +0800

    update status to smart select

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,25 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7({
-    // If it is webapp, we can enable hash navigation:
-    pushState: true,
-     // Hide and show indicator during ajax requests
-    onAjaxStart: function (xhr) {
-        myApp.showIndicator();
-    },
-    onAjaxComplete: function (xhr) {
-        myApp.hideIndicator();
-    },
-    onPageBeforeInit: function (app, page) {
-        console.log('init onPageInit', user);
-        if(user.id == ''){
-            console.log('user not login');
-            app.loginScreen();
-        }
-    },
-    onPageInit: function (app, page) {
-        //設定sidebar使用者的名稱
-        document.getElementById('user-name').innerHTML = user.name;
+  // If it is webapp, we can enable hash navigation:
+  pushState: true,
+  // Hide and show indicator during ajax requests
+  onAjaxStart: function (xhr) {
+    myApp.showIndicator();
+  },
+  onAjaxComplete: function (xhr) {
+    myApp.hideIndicator();
+  },
+  onPageBeforeInit: function (app, page) {
+    console.log('init onPageInit', user);
+    if (user.id == '') {
+      console.log('user not login');
+      app.loginScreen();
     }
+  },
+  onPageInit: function (app, page) {
+    //設定sidebar使用者的名稱
+    document.getElementById('user-name').innerHTML = user.name;
+  }
 });
 
 // We need to use custom DOM library, let's save it to $$ variable:
@@ -27,8 +27,8 @@ var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
 });
 
 // map token
--- app/scripts/status-page.js
+++ app/scripts/status-page.js
@@ -1,11 +1,9 @@
 myApp.onPageInit('status', function (page) {
-  var pickerDevice = myApp.picker({
-    input: '#picker-device',
-    cols: [
-      {
-        textAlign: 'center',
-        values: ['開心', '緊急', '難過', 'GG']
-      }
-    ]
-  });
+  // document.getElementsByClassName('item-after').value
+  console.log('status onPageInit', $$('#sel').text())
+});
+
+myApp.onPageBack('status', function (page) {
+  // document.getElementsByClassName('item-after').value
+  console.log('status onPageBack', $$('#sel').text())
 });
