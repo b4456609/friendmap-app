45a2abcab4bd46c8159c2f7b7851eb88361d6f88
Bernie <b4456609@gmail.com>
2016-06-15 17:04:29 +0800

    status update

--- app/scripts/status-page.js
+++ app/scripts/status-page.js
@@ -3,8 +3,17 @@ myApp.onPageInit('status', function (page) {
   console.log('status onPageInit', $$('#sel').text())
 });
 
-myApp.onPageBack('status', function (page) {
+myApp.onPageBeforeRemove('status', function (page) {
   // document.getElementsByClassName('item-after').value
   console.log('status onPageBack', $$('#sel').text())
-  group.updateSelfStatus($$('#sel').text());
+  group.updateSelfStatus(statusToValue($$('#sel').text()));
 });
+
+function statusToValue(val){
+  if(val === '開心')
+    return 'happy';
+  if(val === '緊急')
+    return 'warning';
+  if(val === 'GG')
+    return 'gg';
+}
